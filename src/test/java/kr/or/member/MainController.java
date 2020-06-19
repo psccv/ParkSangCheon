a24f03cd9dc1f1b28f6200bb5b8ce3b0118d1f1b
home <home@home-PC>
2020-06-19 17:36:19 +0900

    hsql, mysql jdbc test

--- src/test/java/kr/or/member/MainController.java
+++ src/test/java/kr/or/member/MainController.java
@@ -15,62 +15,51 @@ import kr.or.member.MemberService;
 import kr.or.member.MemberVO;
 
 public class MainController {
+
 	private static final String TIME_SERVER = "pool.ntp.org";
-	
+
 	public static void main(String[] args) throws IOException {
-		System.out.println("------------------------------------");
-		System.out.println("클래스를 사용한 사용자정의 자료형 소스");
-		// =========================================== 
-        // 클래스 연습: 사용자 정의 자료형 사용하기(아래)
-        // ===========================================
-		// MemberVO 클래스를 사용하기 위해 new연산자를 이용해 객체(instance) 생성
+		System.out.println("클래스형 변수-사용자정의 자료형");
+		//MemberVO 클래스를 실행하기 위해 new 연산자 사용
 		MemberVO m1 = new MemberVO();
-		
-		// setter 메소드를 이용해 해당 인스턴스 변수(필드)에 값을 넣어준다.
-		m1.setName("홍길동");
+		// setter 메서드를 이용해 해당 인스턴스 변수에 값을 넣는다.
+		m1.setName("박상천");
 		m1.setAge(45);
 		m1.setPhoneNum("000-0000-0000");
-
-		// 마찬가지로 다른 MemberVO 객체(instance)를 생성해서 값을 넣어준다.
+		// 마찬가지로 다른 MemberVO객체(instance)를 생성해서 값을 넣는다.
 		MemberVO m2 = new MemberVO();
 		MemberVO m3 = new MemberVO();
-
-		m2.setName("성춘향");
+		m2.setName("박상천");
 		m2.setAge(100);
 		m2.setPhoneNum("111-1111-1111");
-
-		m3.setName("각시탈");
+		m3.setName("박상천");
 		m3.setAge(3);
 		m3.setPhoneNum("222-2222-2222");
-		System.out.println(m1.toString());
-		System.out.println(m2.toString());
-		System.out.println(m3.toString());
-		// 크기가 3인 MemberVO클래스를(사용자정의자료형) 배열객체로 선언하면서, new연산자를 이용해 객체로 생성한다.
+		
+		//크기가 3인 MemberVO클래스를 배열객체로 선언
 		MemberVO[] members = new MemberVO[3];
-		// MemberVO배열클래스에 각각의 객체(instance) m1, m2, m3를 넣어준다.
+		//MemberVO[]배열클래스 참조index에 m1,m2,m3를 넣는다.
 		members[0] = m1;
 		members[1] = m2;
 		members[2] = m3;
+		//출력: MemberService 클래스를 사용
+		MemberService ms = new MemberService();
+		ms.printMembers(members);
 		
-		// 출력: MemberService 클래스를 사용하기 위해 new연산자를 이용해 객체(instance) 생성
-		MemberService t = new MemberService();
-		t.printMembers(members); // 매개변수로 사용자정의자료형인 MemberVO배열을 넘겨준다.
-		
-		// MainControl 클래스에 아래 내용 추가하 후 에러 상황 처리하기를 이용해서 외부 라이브러리 사용방법 확인
-		 
+		/*
+		 *  MainControl 클래스에 아래 내용 추가하 후 에러 상황 처리하기를 이용해서 외부 라이브러리 사용방법 확인
+		 */
 		System.out.println("-서버타임 출력하기-");
 		NTPUDPClient timeClient = new NTPUDPClient();
 		timeClient.setDefaultTimeout(1000);
 		try {
 			timeClient.open();
-			System.out.println("try 실행");
-			throw new SocketException();//강제 오류 예외처리 (Exception) 처리
+			//throw new RuntimeException();//강제 오류예외(Exception)처리
 		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			System.out.println("catch 실행");
+			System.out.println("open메서드 실행 오류");
 			e.printStackTrace();
 		} finally {
-			System.out.println("finally는 오류에 상관없이 실행됩니다.");
+			System.out.println("이부분은 성공/오류에 상관없이 항상 실행됩니다.");
 		}
 		InetAddress address = InetAddress.getByName(TIME_SERVER);
 	    TimeInfo timeInfo = timeClient.getTime(address);
@@ -84,7 +73,7 @@ public class MainController {
 	    
 	    System.out.println("Server 시간: " + localDateTime);//서버 시간!
 	    System.out.println("로컬 PC 시간: " + LocalDateTime.now());
-        
+
 	}
 
 }
